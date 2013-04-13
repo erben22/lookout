be220e67608a483fed14e5018695cf59216776a3
R. Cody Erben (lookout) <erben22@mtnaircomputer.net>
2013-04-13 17:46:54 -0600

    Solving for sample set.

--- codejam/2013/qual-round/fair-and-square/main.cpp
+++ codejam/2013/qual-round/fair-and-square/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <string>
+#include <math.h>
 
 const int kMaxStringSize = 1000;
 
@@ -9,8 +10,6 @@ bool IsPalindrome(char* pTestString)
 
     std::string testString(pTestString);
     int stringLength = testString.length();
-
-    printf("testString: %s\n", testString.c_str());
 
     if (1 == stringLength)
     {
@@ -22,9 +21,9 @@ bool IsPalindrome(char* pTestString)
 
         for (int stringIndex = 0; (stringLength / 2) > stringIndex;  ++stringIndex)
         {
-            printf("Testing stringIndex[%d]: testString[stringIndex]: %c, testString[%d]: %c\n",
+            /*printf("Testing stringIndex[%d]: testString[stringIndex]: %c, testString[%d]: %c\n",
                 stringIndex, testString[stringIndex], (stringLength - 1) - stringIndex, 
-                    testString[(stringLength - 1) - stringIndex]);
+                    testString[(stringLength - 1) - stringIndex]);*/
 
             if (testString[stringIndex] != testString[(stringLength - 1) - stringIndex])
             {
@@ -34,14 +33,42 @@ bool IsPalindrome(char* pTestString)
         }
     }
 
+    /*
+    if (isPalindrome)
+    {
+        printf("%s: %s is %sa palindrome.\n", __FUNCTION__, testString.c_str(), 
+            isPalindrome ? "" : "not ");
+    }
+    */
+
     return isPalindrome;
 }
 
-bool IsSquare(int testNumber)
+bool GetSquareRoot(int testNumber, int& square)
 {
-    bool isSquare = false;
+    bool foundIntegerSquare = false;
+    int squareRoot = sqrt(testNumber);
+    
+    if ((pow(squareRoot - 1, 2)) == testNumber)
+    {
+        printf("Square root of %d is %d\n", testNumber, squareRoot - 1);
+        square = squareRoot - 1;
+        foundIntegerSquare = true;
+    }
+    else if ((pow(squareRoot, 2)) == testNumber)
+    {
+        printf("Square root of %d is %d\n", testNumber, squareRoot);
+        square = squareRoot;
+        foundIntegerSquare = true;
+    }
+    else if ((pow(squareRoot + 1, 2)) == testNumber)
+    {
+        printf("Square root of %d is %d\n", testNumber, squareRoot + 1);
+        square = squareRoot + 1;
+        foundIntegerSquare = true;        
+    }
 
-    return isSquare;
+    return foundIntegerSquare;
 }
 
 int main()
@@ -63,9 +90,17 @@ int main()
 
             if (0 <= sprintf(testString, "%d" ,testIndex))
             {
-                if (IsPalindrome(testString) && IsSquare(testIndex))
+                if (IsPalindrome(testString))
                 {
-                    ++numFairNSquares;
+                    int squareRoot = 0;
+                    char sqrtString[kMaxStringSize] = {0};
+
+                    if (GetSquareRoot(testIndex, squareRoot) && 
+                            (0 <= sprintf(sqrtString, "%d" ,squareRoot)) && 
+                                IsPalindrome(sqrtString))
+                    {
+                        ++numFairNSquares;
+                    }
                 }
             }
         }
