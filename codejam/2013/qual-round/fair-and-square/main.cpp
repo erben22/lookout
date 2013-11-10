7636fde8e5c4f0dc4313ef5f7334f9b4862786e8
R. Cody Erben (lookout) <erben22@mtnaircomputer.net>
2013-11-10 15:17:14 -0700

    Checking in status of things as of 11/10/2013.

--- codejam/2013/qual-round/fair-and-square/main.cpp
+++ codejam/2013/qual-round/fair-and-square/main.cpp
@@ -21,10 +21,6 @@ bool IsPalindrome(char* pTestString)
 
         for (int stringIndex = 0; (stringLength / 2) > stringIndex;  ++stringIndex)
         {
-            /*printf("Testing stringIndex[%d]: testString[stringIndex]: %c, testString[%d]: %c\n",
-                stringIndex, testString[stringIndex], (stringLength - 1) - stringIndex, 
-                    testString[(stringLength - 1) - stringIndex]);*/
-
             if (testString[stringIndex] != testString[(stringLength - 1) - stringIndex])
             {
                 isPalindrome = false;
@@ -32,14 +28,6 @@ bool IsPalindrome(char* pTestString)
             }
         }
     }
-
-    /*
-    if (isPalindrome)
-    {
-        printf("%s: %s is %sa palindrome.\n", __FUNCTION__, testString.c_str(), 
-            isPalindrome ? "" : "not ");
-    }
-    */
 
     return isPalindrome;
 }
@@ -51,19 +39,16 @@ bool GetSquareRoot(int testNumber, int& square)
     
     if ((pow(squareRoot - 1, 2)) == testNumber)
     {
-        printf("Square root of %d is %d\n", testNumber, squareRoot - 1);
         square = squareRoot - 1;
         foundIntegerSquare = true;
     }
     else if ((pow(squareRoot, 2)) == testNumber)
     {
-        printf("Square root of %d is %d\n", testNumber, squareRoot);
         square = squareRoot;
         foundIntegerSquare = true;
     }
     else if ((pow(squareRoot + 1, 2)) == testNumber)
     {
-        printf("Square root of %d is %d\n", testNumber, squareRoot + 1);
         square = squareRoot + 1;
         foundIntegerSquare = true;        
     }
@@ -105,6 +90,6 @@ int main()
             }
         }
 
-        printf("Case #%d (%d...%d): %d\n", inputIndex, inFloor, inCeil, numFairNSquares);
+        printf("Case #%d: %d\n", inputIndex, numFairNSquares);
     }
 }
---
+++ codejam/temp/main.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <stdint.h>
+
+int64_t PaintUsed(int64_t radius)
+{
+    return ((int)radius * (int)radius);
+}
+
+int PaintRing(int64_t radius, int64_t paint)
+{
+    int numRings = 0;
+
+    while (paint >= 0)
+    {
+        paint -= PaintUsed(radius);
+        if (paint > 0)
+        {
+            ++numRings;
+        }
+        else if (paint == 0)
+        {
+            ++numRings;
+        }
+
+        radius += 2;
+    }
+
+    return numRings;
+}
+
+int main()
+{
+    freopen("A-small-attempt0.in", "r" , stdin); 
+    freopen("data.out", "w" , stdout);
+    std::cout << std::fixed << std::setprecision(12);
+    
+    int64_t numInput = 0; 
+    std::cin >> numInput;
+
+    for (int inputIndex = 1; numInput >= inputIndex; ++inputIndex) 
+    {
+        int64_t radius = 0;
+        int64_t paint = 0;
+        std::cin >> radius >> paint;
+
+        std::cout << "Case #" << inputIndex << ": " << PaintRing(radius, paint) << std::endl;
+    }
+
+    return 0;
+}
+
