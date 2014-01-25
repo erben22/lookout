749fc6af22333d96521bc5c11300fe45ad2ccb3d
rerben <cody.erben@hp.com>
2014-01-25 00:28:13 -0700

    Adding a console application (C#) that will process ATCSMon image captures, preparing them for viewing via a web interface.

---
+++ ATCSMon/ATCSMon-Image-Processor/Application/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATCSMon_Image_Processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
