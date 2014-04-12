cf60293911b44c1d91cc3ab439627fbd1b68dd09
R. Cody Erben (lookout) <erben22@mtnaircomputer.net>
2014-04-12 15:44:31 -0600

    Adding a version of the Magic Trick solution.  This one does not find the solution yet, but parses all the dat and sets up the findCard method with the necessary data to find the answer.

---
+++ codejam/2014/qual-round/magic-trick/CodeJam2014-MagicTrick/CodeJam2014-MagicTrick/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CodeJam2014-MagicTrick
+//
+//  Created by R. Cody Erben on 04/12/2014.
+//  Copyright (c) 2014 CodeBets. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
---
+++ codejam/2014/qual-round/magic-trick/main.cpp
@@ -0,0 +1,109 @@
+#include <cstdio>
+#include <vector>
+#include <string>
+
+void dumpData(std::vector<unsigned int>& data)
+{
+    std::vector<unsigned int>::const_iterator itor = data.begin();
+    
+    printf("Dumping data...\n");
+    
+    while (data.end() != itor)
+    {
+        printf("%u ", *itor);
+        itor++;
+    }
+    
+    printf("\n");
+}
+
+std::string findCard(std::vector<unsigned int>& firstRow, std::vector<unsigned int>& secondRow)
+{
+    const char kBadMagician[] = "Bad magician!";
+    const char kCheaterCheater[] = "Volunteer cheated!";
+    
+    std::string result(kBadMagician);
+    
+    return result;
+}
+
+int main()
+{
+    unsigned int numTestCases = 0;
+    scanf("%u\n", &numTestCases);
+    //printf("Processing %u test cases...\n", numTestCases);
+
+    int trickResult = 0;
+    
+    for (int inputIndex = 1; numTestCases >= inputIndex; ++inputIndex)
+    {
+        const unsigned int kMaxTablesRows = 4;
+        const unsigned int kMaxRowEntries = 4;
+        
+        unsigned int answerRow1 = 0;
+        scanf("%u\n", &answerRow1);
+        --answerRow1;
+        //printf("For first table, number is in row %u.\n", answerRow1+1);
+
+        std::vector<unsigned int> cardRow1;
+        
+        for (int row = 0; kMaxTablesRows > row; ++row)
+        {
+            if (row == answerRow1)
+            {
+                for (int column = 0; kMaxRowEntries > column; ++column)
+                {
+                    unsigned int value = 0;
+                    scanf("%u", &value);
+                    cardRow1.push_back(value);
+                    //printf("Read %u into the array.\n", value);
+                }
+            }
+            else
+            {
+                //printf("Skipping row(%u).\n", row);
+                unsigned int dummyValue1 = 0;
+                unsigned int dummyValue2 = 0;
+                unsigned int dummyValue3 = 0;
+                unsigned int dummyValue4 = 0;
+                scanf("%u %u %u %u\n", &dummyValue1, &dummyValue2, &dummyValue3, &dummyValue4);
+            }
+        }
+
+        dumpData(cardRow1);
+        
+        unsigned int answerRow2 = 0;
+        scanf("%u\n", &answerRow2);
+        --answerRow2;
+        //printf("For second table, number is in row %u.\n", answerRow2+1);
+
+        std::vector<unsigned int> cardRow2;
+        
+        for (int row = 0; kMaxTablesRows > row; ++row)
+        {
+            if (row == answerRow2)
+            {
+                for (int column = 0; kMaxRowEntries > column; ++column)
+                {
+                    unsigned int value = 0;
+                    scanf("%u", &value);
+                    cardRow2.push_back(value);
+                    //printf("Read %u into the array.\n", value);
+                }
+            }
+            else
+            {
+                //printf("Skipping row(%u).\n", row);
+                unsigned int dummyValue1 = 0;
+                unsigned int dummyValue2 = 0;
+                unsigned int dummyValue3 = 0;
+                unsigned int dummyValue4 = 0;
+                scanf("%u %u %u %u\n", &dummyValue1, &dummyValue2, &dummyValue3, &dummyValue4);
+            }
+        }
+        
+        dumpData(cardRow2);
+
+        printf("Case #%u: %s\n", inputIndex, findCard(cardRow1, cardRow2).c_str());
+    }
+}
