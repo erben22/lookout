420d2cd2f0e0e18c6fd749a1d42d0764edd5ad5d
R. Cody Erben (lookout) <erben22@mtnaircomputer.net>
2014-05-03 11:22:24 -0600

    Adding working progress of the repeater solution.  Not complete and does not solve the problem at this time.

--- codejam/2014/round-1b/repeater/main.cpp
+++ codejam/2014/round-1b/repeater/main.cpp
@@ -1,12 +1,12 @@
 #include <cstdio>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 void GatherInput(std::vector<std::string>& testCaseData)
 {
     unsigned int numStrings = 0;
     scanf("%u\n", &numStrings);
-    //printf("For this test case, numstrings is %u.\n", numStrings);
 
     for (int dataIndex = 0; numStrings > dataIndex; ++dataIndex)
     {
@@ -19,10 +19,83 @@ void GatherInput(std::vector<std::string>& testCaseData)
     }
 }
 
+void GetUniqueCharacters(const std::string& testCaseString, 
+    std::vector<char>& uniqueChars)
+{
+    for (int stringIndex = 0;  testCaseString.length() > stringIndex; ++stringIndex)
+    {
+        if (uniqueChars.end() == 
+            find(uniqueChars.begin(), uniqueChars.end(), 
+                testCaseString.at(stringIndex)))
+        {
+            uniqueChars.push_back(testCaseString.at(stringIndex));
+        }
+    }
+
+    
+    printf("For string %s, the unique chars are:\n", testCaseString.c_str());
+    std::vector<char>::const_iterator uniqueItor = uniqueChars.begin();
+
+    while (uniqueChars.end() != uniqueItor)
+    {
+        printf("%c", (*uniqueItor));
+        uniqueItor++;
+    }
+    printf("\n");
+    
+}
+
+bool AllDataEqual(std::vector<std::string>& testCaseData)
+{
+    std::vector<std::string>::const_iterator itor = testCaseData.begin();
+
+    while (testCaseData.end() != itor)
+    {
+        std::vector<std::string>::const_iterator nextItor = itor + 1;
+
+        if (testCaseData.end() != nextItor)
+        {
+            printf("Comparing %s to %s\n", (*itor).c_str(), (*nextItor).c_str());
+            if (0 != (*itor).compare((*nextItor)))
+            {
+                return false;
+            }
+        }
+
+        itor++;
+    }
+
+    return true;
+}
+
+bool ProcessStrings(std::vector<std::string>& testCaseData, unsigned int& numMoves)
+{
+    bool wonGame = false;
+
+    // To have a possibility of winning, all the individual characters
+    // must exist in both strings.  The number of each character's
+    // appearance is immaterial.
+
+    std::vector<char> uniqueCharsFirst;
+    std::vector<char> uniqueCharsSecond;
+
+    GetUniqueCharacters((*itor), uniqueCharsFirst);
+    GetUniqueCharacters((*nextItor), uniqueCharsSecond);
+
+
+
+    if (AllDataEqual(testCaseData))
+    {
+        wonGame = true;
+    }
+
+    return wonGame;
+}
+
 int main()
 {
     // Snag the number of test cases and start processing:
-    
+
     unsigned int numTestCases = 0;
     scanf("%u\n", &numTestCases);
 
@@ -43,8 +116,17 @@ int main()
         }
         // Debug Output -- end.
 
+        // Process this test case and determine the results:
 
+        unsigned int numMovesToWin = 0;
 
-        printf("Case #%u: %s\n", inputIndex, "blah");
+        if (ProcessStrings(testCaseData, numMovesToWin))
+        {
+            printf("Case #%u: %u\n", inputIndex, numMovesToWin);
+        }
+        else
+        {
+            printf("Case #%u: %s\n", inputIndex, "Fegla Won");
+        }
     }
 }
